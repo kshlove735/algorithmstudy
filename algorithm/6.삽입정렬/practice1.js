69289bc1ad43b276fb3b95d546e1293acaa7bc3c
shKim <kshlove735@gmail.com>
2022-11-03 22:50:04 +0900

    삽입정렬

--- algorithm/6.삽입정렬/practice1.js
+++ algorithm/6.삽입정렬/practice1.js
@@ -4,6 +4,9 @@
 //  올바른 위치가 아니라면 계속해서 정렬된 부분을 거치며 반복
 //  배열을 정렬할 때까지 반복하고 정렬된 배열 return
 
+// Big O
+// - worst case : O(n²)
+
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
