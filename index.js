bc4fd4f3c725873547b2cd7cb9a1ff32053b4a9d
Seunghyun Kim <kshlove735@suhwooms.com>
2022-11-03 20:29:50 +0900

    삽입정렬 고민

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-// 가장 작은 값을 저장할 수 있는 변수 설정 -> 첫번째는 첫번째 index로
-// 가장 작은 수를 찾을 때 까지 비교
-// 최소 값을 찾으면 해당 index를 변수에 저장
-// 최소 값 시작 값이 아니라면 둘을 swap
-// 정렬이 다 될때 까지 반복
+//  배열의 두번째 요소부터 시작→ 첫번째 요소는 정렬하지 않아도 되기 때문
+//  두번째 값을 선택해서 앞에 있는 값과 비교하여 필요하다면 swap
+//  계속해서 다음 요소를 진행한다.
+//  올바른 위치가 아니라면 계속해서 정렬된 부분을 거치며 반복
+//  배열을 정렬할 때까지 반복하고 정렬된 배열 return
 
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let minIndex = i;
-
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) minIndex = j;
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) {
+      continue;
     }
-    // swap
-    if (i !== minIndex) {
-      let temp = arr[minIndex];
-      arr[minIndex] = arr[i];
-      arr[i] = temp;
-    }
+    console.log(arr[i]);
   }
-  return arr;
 }
-
-console.log(selectionSort([0, 2, 34, 22, 10, 19, 17]));
-// console.log(selectionSort([5, 3, 4, 1, 2]));
-// console.log(selectionSort([2, 3, 4, 5, 1]));
