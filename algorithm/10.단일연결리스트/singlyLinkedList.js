6ee73fdba284780febc8d03b92a44b08570a495e
Seunghyun Kim <kshlove735@suhwooms.com>
2022-11-16 20:57:10 +0900

    단일연결리스트 insert()

--- algorithm/10.단일연결리스트/singlyLinkedList.js
+++ algorithm/10.단일연결리스트/singlyLinkedList.js
@@ -119,6 +119,31 @@ class SinglyLinkedList {
     // node를 찾을 수 없으면 return false
     return false;
   }
+
+  // 원하는 위치에 node 추가
+  insert(index, val) {
+    // index가 0보다 작거나 length보다 크면 return false
+    if (index < 0 || index > this.length) return false;
+    // length 보다 크거나 "같은" 이 아닌 이유는 length와 "같으면" 마지막 node에 push() 그리고 무조건 true 리턴
+    if (index === this.length) return !!this.push(val);
+    // index가 0 이면, unshift() 매소드 이용 그리고 무조건 true 리턴
+    if (index === 0) return !!this.unshift(val);
+
+    // 그렇지 않으면 get() 메소드를 할용하여 index-1 위치의 node 찾아온다.
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+
+    // 새로운 node를 생성하여
+    let newNode = new Node(val);
+
+    // 이전 node.next에 추가할 node 연결
+    prev.next = newNode;
+    // 추가한 node.next에 원래 해당 위치에 있었던 node 연결
+    newNode.next = temp;
+    // length 1증가
+    this.length++;
+    return true;
+  }
 }
 // let first = new Node("Hi");
 // first.next = new Node("there");
@@ -148,7 +173,8 @@ list.push(":)");
 // console.dir(list.shift(), { depth: null });
 
 // console.dir(list.unshift(99), { depth: null });
-
 // console.dir(list.get(2), { depth: null });
-console.dir(list.set(5, "YES"), { depth: null });
+// console.dir(list.set(5, "YES"), { depth: null });
+console.dir(list.insert(5, 100), { depth: null });
+
 console.dir(list, { depth: null });
