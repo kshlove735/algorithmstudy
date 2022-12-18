1e351bbb42719fc87ff9f5ccd4933e677f185d64
shKim <kshlove735@gmail.com>
2022-12-19 00:27:20 +0900

    Weighted Graph

---
+++ algorithm/19.다익스트라알고리즘/Dijkstra’s_Algorithm.js
@@ -0,0 +1,23 @@
+// Undirected Graph
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2, weight) {
+    if (this.adjacencyList[vertex1]) this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    if (this.adjacencyList[vertex2]) this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+}
+
+let g = new WeightedGraph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addEdge("A", "B", 9);
+g.addEdge("A", "C", 5);
+g.addEdge("B", "C", 7);
+console.dir(g, { depth: null });
